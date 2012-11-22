aa86c1e4e7686b94121efc7839916676e0c7a9a4
Raphael Javaux <raphaeljavaux@gmail.com>
2012-11-22 16:08:06 +0100

    polygon writing and reading

--- Programming techniques/polygon/main.c
+++ Programming techniques/polygon/main.c
@@ -1,79 +1,36 @@
-/* Projet de programmation: Calculs sur des entiers en precision arbitraire.
- * Raphael Javaux - Septembre 2012.
+/* Programming techniques: Polygon
+ * Raphael Javaux - November 2012.
  *
- * Ce fichier effectue les tests sur les exemples definis dans l'enonce du
- * cours.
+ * This file read a list of points from stdin and output the corresponding
+ * simple polygon on stdin.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 #include <assert.h>
 
-#include "integers.h"
-#include "comparaisons.h"
-#include "arithmetic.h"
-
-/** Teste si fct appliquee a e1 et e2 donne le meme entier que celui represente
- * par la chaine de caracteres res.
- * @pre e1, e2 et res sont des chaines de caracteres codant des entiers.
- * @post Provoque une assertion si le resultat n'est pas correct. Sinon,
- *       affiche le calcul.
- */
-void testInteger(
-   char *e1, char *e2, Integer (*fct)(Integer e1, Integer e2), char *nomFct,
-   char *res
-);
+#include "polygon.h"
 
 int main(void)
 {
-   /* Verifie que ValeurCalcul et ValeurNoeud peuvent contenir, respectivement
-    * les valeurs dans [0; 9999] et [0; 9999^2]. */
-   assert (USHRT_MAX >= 9999); // Toujours vrai, le standard impose >= 65535.
-   assert ( // Toujours vrai, le standard impose >= (65536^2 - 1).
-      ULONG_MAX / 9999 >= 9999
-   );
-   printf(
-      "Les types de la machine hote sont suffisament importants pour contenir "
-      "les valeurs necessaires aux calculs.\n"
-   );
+    // Reads the number of points given as input.
+    int n;
+    scanf("%d", &n);
 
-   // Teste les exemples du cours
-   testInteger("4322193", "38200107673", addition, "+", "38204429866");
-   testInteger(
-      "4322193", "-38200107673", multiplication, "*", "-165108237983486889"
-   );
-   testInteger("4325", "0", addition, "+", "4325");
-   testInteger("4325", "0", multiplication, "*", "0");
-   testInteger("38200107673", "4322193", subtraction, "-", "38195785480");
-   testInteger("38200107673", "4322193", division, "/", "8838");
-   testInteger("38200107673", "4325", addition, "+", "38200111998");
-   testInteger("38200107673", "4325", multiplication, "*", "165215465685725");
-   testInteger("38200107673", "-4325", multiplication, "*", "-165215465685725");
-   testInteger("38200107673", "-4325", subtraction, "-", "38200111998");
-   testInteger("38200107673", "4325", division, "/", "8832394");
+    // Reads and stores the points.
+    POINT *points = malloc(sizeof (POINT) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%lf %lf", &points[i].x, &points[i].y);
 
-   return 0;
-}
+    // Sorts the polygon and write the result to stdout.
+    polygon(points, n);
+    printf("%d\n", n + 1);
+    for (int i = 0; i < n; i++)
+        printf("%lf %lf\n", points[i].x, points[i].y);
+    printf("%lf %lf\n", points[0].x, points[0].y);
 
-void testInteger(
-   char *e1, char *e2, Integer (*fct)(Integer e1, Integer e2), char *fctName,
-   char *res
-)
-{
-   Integer e_e1 = readInteger(e1)
-         , e_e2 = readInteger(e2)
-         , e_res = readInteger(res);
+    free(points);
 
-   Integer e_compute = fct(e_e1, e_e2);
-
-   assert(equals(e_res, e_compute));
-
-   freeInteger(e_e1);
-   freeInteger(e_e2);
-   freeInteger(e_res);
-   freeInteger(e_compute);
-
-   printf("%s %s %s = %s\n", e1, fctName, e2, res);
+    return 0;
 }
--- Programming techniques/polygon/polygon.c
+++ Programming techniques/polygon/polygon.c
@@ -5,9 +5,17 @@
  * from an unordered set of points.
  */
 
+#define _GNU_SOURCE
+
 #include <assert.h>
 
 #include "polygon.h"
+
+// This function compares two points by their slopes relative to a third point
+// (named first).
+// This function is used by the qsort_r() sorting function as a comparison
+// routine.
+int point_cmp(const void *v_pt1, const void *v_pt2, void* v_first);
 
 void polygon(POINT points[], size_t n)
 {
@@ -18,12 +26,14 @@ void polygon(POINT points[], size_t n)
     // Takes the first point from the vector and uses it as the first point of
     // the polygon. Sort the remainder of the vector by comparing the relative
     // slope of each point to this first point.
-    qsort_r(points + 1, n - 1, point_cmp, (void *) points);
+    qsort_r(points + 1, n - 1, sizeof (POINT), point_cmp, (void *) points);
 }
 
-int point_cmp(void *v_pt1, void *v_pt2, void* v_first)
+int point_cmp(const void *v_pt1, const void *v_pt2, void* v_first)
 {
-    POINT pt1 = *v_pt1, pt2 = *v_pt2, first = *v_first;
+    POINT pt1 = *(POINT *) v_pt1
+        , pt2 = *(POINT *) v_pt2
+        , first = *(POINT *) v_first;
 
     // Computes the coordinates of the two points relative to the first point.
     double x1 = pt1.x - first.x, y1 = pt1.y - first.y
@@ -32,9 +42,10 @@ int point_cmp(void *v_pt1, void *v_pt2, void* v_first)
     // Compares the slope between two points.
     // Comparing y1 / x1 to y2 / x2 is the same as comparing y1 * x2 to y2 * x1.
     // By replacing divisions by multiplications, we strongly reduce the number
-    // of CPU cycles needed by the comparison (from ~40 to ~5 cycles on a
+    // of CPU cycles needed by the comparison (from ~90 to ~20 cycles on a
     // new Core processor).
     if (y1 * x2 < y2 * x1)
+//     if (y1 / x1 < y2 * x1)
         return -1;
     else
         return 1;
@@ -49,7 +60,8 @@ int point_cmp(void *v_pt1, void *v_pt2, void* v_first)
 
     // We can go deeper in optimisation by replacing the conditional expression
     // by this expression :
-    // return *((long long *) &(y1 * x2 - y2 * x1))
+    double diff = y1 * x2 - y2 * x1;
+    return (int) *((long long *) &diff);
     //
     // This is easy to understand if you know that :
     //    1. the subtraction between two numbers will return a negative number
@@ -65,6 +77,6 @@ int point_cmp(void *v_pt1, void *v_pt2, void* v_first)
     // This reduces the number of cycles needed by the conditional expression by
     // half on a modern processor, in addition to remove potential branch
     // miss-predictions. But this "hack" only works if the sign bit is on the
-    // same position on both two types, which is not defined in the C language
+    // same position on both types, which is not defined in the C language
     // specification.
 }
