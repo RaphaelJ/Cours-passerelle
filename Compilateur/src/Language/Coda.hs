a65a3ead526cc3e1488a35680676f35574d4ce38
Raphael Javaux <raphaeljavaux@gmail.com>
2013-03-03 22:59:25 +0100

    abstract syntax tree

--- Compilateur/src/Language/Coda.hs
+++ Compilateur/src/Language/Coda.hs
@@ -1,1 +1,10 @@
-Language.Coda
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
+
+import Language.Coda.Parser (parse)
+
+main = TL.interact (TL.pack . showAST . parse "stdin")
+  where
+    showAST (Left err)  = show err
+    showAST (Right ast) = show $ length ast
+
