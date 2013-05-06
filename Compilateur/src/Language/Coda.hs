a2c8ad91837213b58c809215c4ccf1d6e87f0679
Raphael Javaux <raphaeljavaux@gmail.com>
2013-05-06 10:52:16 +0200

    preprocessor

--- Compilateur/src/Language/Coda.hs
+++ Compilateur/src/Language/Coda.hs
@@ -2,8 +2,12 @@ import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
 
 import Language.Coda.Parser (parse)
+import Language.Coda.Preprocessor (preprocess)
 
 main :: IO ()
 main = do
-    TL.interact (TL.pack . show . parse "stdin")
+--     TL.interact (TL.pack . show . parse "stdin")
+--     TL.interact (preprocess)
+    (preprocess . TL.replicate 100000) `fmap` TL.readFile "quicksort.coda" >>= TL.writeFile "/dev/null"
+--     (TL.unwords . TL.lines . TL.replicate 100000) `fmap` TL.readFile "quicksort.coda" >>= TL.writeFile "/dev/null"
     putStrLn ""
--- Compilateur/src/Language/Coda/Preprocessor.hs
+++ Compilateur/src/Language/Coda/Preprocessor.hs
@@ -1,1 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Language.Coda.Preprocessor (preprocess) where
+
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Builder (Builder, toLazyText, fromLazyText)
+import Control.Arrow (second)
+import Data.Monoid ((<>))
+
+-- preprocess :: T.Text -> T.Text
+-- preprocess = T.unlines . map preprocessor . T.lines
+-- 
+-- preprocessor :: T.Text -> T.Text
+-- preprocessor = T.takeWhile (/= '#')
+
+preprocess :: T.Text -> T.Text
+preprocess = 
+    toLazyText . preprocessor
+  where
+    preprocessor txt | T.null txt = fromLazyText txt
+                     | otherwise  =
+        let (before, after) = second (T.dropWhile (/= '\n')) $ T.break (== '#') txt
+        in fromLazyText before <> preprocessor after
+
+-- preprocess :: T.Text -> T.Text
+-- preprocess = preprocessor
+--   where
+--     preprocessor t | T.null t  = t
+--                    | otherwise =
+--         let (l, t') = T.break (== '#') t
+--             t'' = T.dropWhile (/= '\n') t'
+--         in l `T.append` preprocessor t''
